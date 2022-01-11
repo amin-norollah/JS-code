8e947decb3783e70f5f2938c46f32ba6409cece9
Amin Norollah <amin.norollah@gmail.com>
2022-01-12 00:34:47 +0330

    Preload images
    for better performance

---
+++ Pages/FlightBooking/preload.js
@@ -0,0 +1,23 @@
+"use strict";
+
+//cache images
+function preloadImages(array) {
+  if (!preloadImages.list) {
+    preloadImages.list = [];
+  }
+  const list = preloadImages.list;
+  for (let i = 0; i < array.length; i++) {
+    const img = new Image();
+    img.onload = function () {
+      const index = list.indexOf(this);
+      if (index !== -1) {
+        // remove image from the array once it's loaded
+        // for memory consumption reasons
+        list.splice(index, 1);
+      }
+    };
+    list.push(img);
+    img.src = array[i];
+  }
+}
+preloadImages(Array.from({ length: 8 }, (_, i) => `img/al_${i + 1}.png`));
